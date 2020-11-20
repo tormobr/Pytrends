1a62dd049c358990c70e6b5206cf91530173e099
tormobr <tormod.brandshoi@gmail.com>
2020-11-20 09:30:35 +0100

    made it more generic

--- server/static/ruter.js
+++ server/static/ruter.js
@@ -21,7 +21,6 @@ function getTimes(stopID, stopName, methods) {
         }
       }
     }`
-    console.log(query)
     var values = {}
     fetch('https://api.entur.io/journey-planner/v2/graphql', {
         method: 'POST',
@@ -40,10 +39,10 @@ function getTimes(stopID, stopName, methods) {
         data.forEach(item => {
             const name = item.destinationDisplay.frontText;
             if (values[name] === undefined){
-                values[name] = [{"time": item.expectedDepartureTime, "method":item.serviceJourney.line.transportMode}]
+                values[name] = [{"time": item.expectedDepartureTime, "method":item.serviceJourney.line.transportMode, "num":item.serviceJourney.line.publicCode}]
             }
             else{
-                values[name].push({"time": item.expectedDepartureTime, "method":item.serviceJourney.line.transportMode})
+                values[name].push({"time": item.expectedDepartureTime, "method":item.serviceJourney.line.transportMode, "num":item.serviceJourney.line.publicCode})
             }
             //if (values[name] == undefined){
                 //values.push({ key: name, value: item.aimedDepartureTime })
@@ -51,17 +50,29 @@ function getTimes(stopID, stopName, methods) {
 
         })
 
+        const header = document.createElement("h1")
+        header.innerHTML = stopName
+        document.body.appendChild(header)
+
         for (var key in values){
-            var tableBody = document.getElementById(values[key][0].method);
+            const method = values[key][0].method
+            var tableBody = document.getElementById(method);
+            if (tableBody === null){
+                tableBody = createTable(method)
+            }
+            console.log("ETF")
             const elem = document.createElement("tr");
             const k = document.createElement("td");
-            const method = values[key][0].method
-            k.innerHTML = key + "(" + method + ")";
+            const number = values[key][0].num
+            const span = document.createElement("td")
+            span.innerHTML = number
+            k.innerHTML = key
+            elem.appendChild(span);
             elem.appendChild(k);
+
             for (var i = 0; i < 4; i++){
                 time = document.createElement("td");
                 const val = values[key][i]
-                console.log(key)
                 if (val){
                     const minutes = (new Date(val.time).getTime() - Date.now()) / (1000 * 60)
                     time.innerHTML = minutes.toFixed(1)
@@ -75,4 +86,24 @@ function getTimes(stopID, stopName, methods) {
 
         }
     })
+
+    function createTable(id){
+        console.log("creating table hree")
+        var table = document.createElement("table")
+        const body = document.body.appendChild(table)
+        table.setAttribute("id", "times")
+        var tableBody = document.createElement("tbody");
+        tableBody.setAttribute("id", id)
+        table.appendChild(tableBody)
+        const row = document.createElement("tr")
+        const rute = document.createElement("th")
+        rute.innerHTML = "Rute"
+        rute.setAttribute("colspan", 2)
+        const time = document.createElement("th")
+        time.setAttribute("colspan", 4)
+        time.innerHTML = "Minutter til Ankomst"
+        tableBody.appendChild(rute)
+        tableBody.appendChild(time)
+        return tableBody
+    }
 }
